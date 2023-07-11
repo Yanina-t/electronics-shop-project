146edfd2f5767a38b83a630e6095dfae128c0345
Yanina-t <vyaninat@gmail.com>
2023-07-11 12:11:17 +0200

    add def and test

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,17 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+# """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+
+def test_calculate_total_price():
+    price = 20
+    quantity = 30
+    assert price * quantity == 600
+
+
+def test_apply_discount():
+    price = 20
+    pay_rate = 0.8
+    price = price * pay_rate
+    assert price == 16
